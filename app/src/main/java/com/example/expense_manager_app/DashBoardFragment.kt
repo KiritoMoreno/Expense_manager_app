7f4eff362f7b2811730b5d60a11675ba3fa8d440
kiritomoreno <moreno.algorithmictrading@gmail.com>
2024-02-24 17:45:29 +0000

    Floating Button Animation

--- app/src/main/java/com/example/expense_manager_app/DashBoardFragment.kt
+++ app/src/main/java/com/example/expense_manager_app/DashBoardFragment.kt
@@ -5,55 +5,79 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DashBoardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DashBoardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // Floating buttons
+    private lateinit var fabMainBtn: FloatingActionButton
+    private lateinit var fabIncomeBtn: FloatingActionButton
+    private lateinit var fabExpenseBtn: FloatingActionButton
+
+    // Floating button textviews
+    private lateinit var fabIncomeTxt: TextView
+    private lateinit var fabExpenseTxt: TextView
+
+    // Boolean
+    private var isOpen = false
+
+    // Animations
+    private lateinit var fadeOpen: Animation
+    private lateinit var fadeClose: Animation
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dash_board, container, false)
+        val myView = inflater.inflate(R.layout.fragment_dash_board, container, false)
+
+        // Connect floating buttons to layout
+        fabMainBtn = myView.findViewById(R.id.fb_main_plus_btn)
+        fabIncomeBtn = myView.findViewById(R.id.income_ft_btn)
+        fabExpenseBtn = myView.findViewById(R.id.expense_ft_btn)
+
+        // Connect floating textviews
+        fabIncomeTxt = myView.findViewById(R.id.income_ft_text)
+        fabExpenseTxt = myView.findViewById(R.id.expense_ft_text)
+
+        fadeOpen = AnimationUtils.loadAnimation(requireActivity(), R.anim.fade_open)
+        fadeClose = AnimationUtils.loadAnimation(requireActivity(), R.anim.fade_close)
+
+        fabMainBtn.setOnClickListener {
+            if (isOpen) {
+                closeFabMenu()
+            } else {
+                openFabMenu()
+            }
+        }
+
+        return myView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DashBoardFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DashBoardFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun openFabMenu() {
+        fabIncomeBtn.startAnimation(fadeOpen)
+        fabExpenseBtn.startAnimation(fadeOpen)
+        fabIncomeBtn.isClickable = true
+        fabExpenseBtn.isClickable = true
+        fabIncomeTxt.startAnimation(fadeOpen)
+        fabExpenseTxt.startAnimation(fadeOpen)
+        fabIncomeTxt.isClickable = true
+        fabExpenseTxt.isClickable = true
+        isOpen = true
+    }
+
+    private fun closeFabMenu() {
+        fabIncomeBtn.startAnimation(fadeClose)
+        fabExpenseBtn.startAnimation(fadeClose)
+        fabIncomeBtn.isClickable = false
+        fabExpenseBtn.isClickable = false
+        fabIncomeTxt.startAnimation(fadeClose)
+        fabExpenseTxt.startAnimation(fadeClose)
+        fabIncomeTxt.isClickable = false
+        fabExpenseTxt.isClickable = false
+        isOpen = false
     }
 }
